5be1fdb82ef61fec0022484923c1c168746e58b7
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-11-15 18:34:21 -0500

    feat: actividad de clase

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const server = app.listen(8080, () => console.log('Server Up'));
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 let fakeData = [
     { fname: "Matias", lname: "Sánchez", age: 21 },
@@ -12,8 +14,21 @@ let fakeData = [
     { fname: "Lautaro", lname: "Vazquez", age: 32 }
 ]
 
+let users = [];
+
 app.get('/', (req, res) => {
     res.render('home', {
         alumnos: fakeData
     })
+})
+
+app.get('/actividad', (req, res) => {
+    res.render('users', {
+        users
+    })
+})
+
+app.post('/users', (req, res) => {
+    users.push(req.body)
+    res.redirect('/actividad')
 })
