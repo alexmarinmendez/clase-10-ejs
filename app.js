5fb9960a275ea731f53301072556627b3c4e3cc4
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-11-15 17:45:20 -0500

    feat: basic express server with ejs templates

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+const server = app.listen(8080, () => console.log('Server Up'));
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
